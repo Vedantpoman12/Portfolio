978bd11b60c6903094210ca529ca55d875ace8cf
Vedantpoman12 <vedant.poman@gmail.com>
2026-09-07 19:20:57 +0530

    style: remove logo from header and footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,4 @@
 import { Container, Row, Col } from "react-bootstrap";
-import logo from "../assets/img/logo.svg";
 import navIcon1 from "../assets/img/nav-icon1.svg";
 import navIconGitHub from "../assets/img/nav-icon-github.svg";
 import navIconResume from "../assets/img/nav-icon-resume.svg";
@@ -9,9 +8,7 @@ export const Footer = () => {
     <footer className="footer">
       <Container>
         <Row className="align-items-center">
-          <Col size={12} sm={6}>
-            <img src={logo} alt="Logo" />
-          </Col>
+
           <Col size={12} sm={6} className="text-center text-sm-end">
             <div className="social-icon">
               <a href="https://github.com/Vedantpoman12" target="_blank" rel="noreferrer" title="GitHub"><img src={navIconGitHub} alt="GitHub" /></a>
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import { Navbar, Nav, Container } from "react-bootstrap";
-import logo from '../assets/img/logo.svg';
+
 import navIcon1 from '../assets/img/nav-icon1.svg';
 import navIconGitHub from '../assets/img/nav-icon-github.svg';
 import navIconResume from '../assets/img/nav-icon-resume.svg';
@@ -36,9 +36,7 @@ export const NavBar = () => {
     <Router>
       <Navbar expand="md" className={scrolled ? "scrolled" : ""}>
         <Container>
-          <Navbar.Brand href="/">
-            <img src={logo} alt="Logo" />
-          </Navbar.Brand>
+
           <Navbar.Toggle aria-controls="basic-navbar-nav">
             <span className="navbar-toggler-icon"></span>
           </Navbar.Toggle>
